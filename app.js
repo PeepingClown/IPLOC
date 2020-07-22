8ab7b3ad197606daf90d14ed652e203cf3400f68
Mayanka Chattopadhyay <jodu892@gmail.com>
2020-07-23 04:55:07 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const http = require('http');
+
+const app = express();
+
+app.set('view engine', "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req,res)=>{
+  res.render("main");
+})
+app.post("/", (req,res) => {
+  const ip = req.body.IP ;
+
+  http.get("http://ip-api.com/json/"+ip, (response)=>{
+    response.on("data", (data)=>{
+      const ipData = JSON.parse(data);
+      res.render("result", {ipData:ipData});
+    })
+  })
+})
+app.listen(3000, ()=>{
+  console.log("Running on Port:3000");
+})
